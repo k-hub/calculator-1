20e1aac6dd79be41a5970a4b01c96c49a75c352a
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:16:10 -0700

    Docstrings

--- arithmetic.py
+++ arithmetic.py
@@ -1,23 +1,55 @@
 def add(num1, num2):
+    """ Adds two numbers together.
+
+    Add num1 and num2 returning the result.
+    """
     return num1 + num2
 
 def subtract(num1, num2):
+    """ Subtract two numbers.
+
+    Subtract num2 from num1 returning the result.
+    """
     return num1 - num2
 
 def multiply(num1, num2):
+    """ Multiply two numbers together.
+
+    Multiply num1 and num2 returning the result.    
+    """
     return num1 * num2
 
 def divide(num1, num2):
+    """ Divide two numbers.
+
+    Divide float(num2) from float(num1) returning the result.
+    """
     return float(num1) / float(num2)
 
 def square(num1):
+    """ Square a number.
+
+    Multiply num1 by itself returning the result.
+    """
     return num1 * num1
 
 def cube(num1):
+    """Cube a number.
+
+    Multiply num1 to itself twice returning the result.
+    """
     return num1 * num1 * num1
 
 def power(num1, num2):
+    """ Raise a number by another number.
+
+    Raise num1 by num2 returning the result.
+    """
     return num1 ** num2
 
 def mod(num1, num2):
+    """ The remainder of the division of two numbers.
+
+    num2 divided by num1 with the result being the remainder.
+    """
     return num1 % num2
